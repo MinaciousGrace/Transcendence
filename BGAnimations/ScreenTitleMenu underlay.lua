c7ae0d74504070df9201eabf3cbf00d030d604fe
Sam Feeney <alphamap@gmail.com>
2016-01-26 21:19:04 -0500

    added link to a "performance" noteskin

--- BGAnimations/ScreenTitleMenu underlay.lua
+++ BGAnimations/ScreenTitleMenu underlay.lua
@@ -4,11 +4,11 @@ local frameX = THEME:GetMetric("ScreenTitleMenu","ScrollerX")-10
 local frameY = THEME:GetMetric("ScreenTitleMenu","ScrollerY")
 
 t[#t+1] = Def.Quad{
-	InitCommand=cmd(draworder,-300;xy,frameX,frameY;zoomto,SCREEN_WIDTH,120;halign,0;diffuse,getMainColor('highlight');diffusealpha,0.5)
+	InitCommand=cmd(draworder,-300;xy,frameX,frameY;zoomto,SCREEN_WIDTH,160;halign,0;diffuse,getMainColor('highlight');diffusealpha,0.5)
 }
 
 t[#t+1] = LoadFont("Common Large") .. {
-	InitCommand=cmd(xy,5,frameY-60;zoom,0.5;valign,1;halign,0;diffuse,getMainColor('highlight'));
+	InitCommand=cmd(xy,5,frameY-80;zoom,0.5;valign,1;halign,0;diffuse,getMainColor('highlight'));
 	OnCommand=function(self)
 		self:settext(getThemeName());
 	end;
